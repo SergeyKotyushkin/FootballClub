5675627312ff6d0f29e0b51e9f6e107f4347158f
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2017-03-10 10:14:10 +0300

    Add PlayerService

--- football-club/app/components/player-list.component.ts
+++ football-club/app/components/player-list.component.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
-
-class PlayerViewModel {
-    constructor(public name: string, public surname: string,
-        public gamesCount: number, public winPercent: string,
-        public position: string) { }
-}
+import { PlayerViewModel } from '../view-models/player.view-model';
+import { PlayerService } from '../services/player.service';
 
 @Component({
     selector: 'player-list-app',
-    templateUrl: './app/templates/player-list.component.html'
+    templateUrl: './app/templates/player-list.component.html',
+    providers: [PlayerService]
 })
 export class PlayerListComponent {
 
-  players: PlayerViewModel[] = [
-     new PlayerViewModel("Sergey", "Kotyushkin", 10, "15%", "attack"),
-     new PlayerViewModel("Player1", "Surname1", 3, "70%", "defender"),
-     new PlayerViewModel("Player2", "Surname2", 24, "56%", "goalkeeper"),
-     new PlayerViewModel("Player3", "Surname3", 109, "39%", "attack"),
-     new PlayerViewModel("Player4", "Surname4", 189, "50%", "defender")
-  ];
+    players: PlayerViewModel[] = [];
 
-  constructor() {
-  }
+    constructor(private playerService: PlayerService) {
+        playerService.getPlayers().then(players => this.players = players);
+    }
 }
---
+++ football-club/app/view-models/player.view-model.ts
@@ -0,0 +1,5 @@
+export class PlayerViewModel {
+    constructor(public id: string, public name: string, public surname: string,
+        public gamesCount: number, public winPercent: string,
+        public position: string) { }
+}
