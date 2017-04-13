20550c6cda111845e6e02179adb0d04fae9cf411
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2017-04-13 10:44:13 +0300

    Update all packages to latest version. Fix build

--- football-club/client/app/app.module.ts
+++ football-club/client/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule }   from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {
@@ -25,6 +26,7 @@ import 'hammerjs';
     imports:      [
       MaterialModule,
       BrowserModule,
+      BrowserAnimationsModule,
       FormsModule,
       AppRoutingModule,
       FlexLayoutModule.forRoot(),
